32840eb6f09e93a40c3e95ab820b5768a942669c
Isaac Tapia <itapia@esri.com>
2023-11-27 15:53:10 -0800

    Create FinishLaunch.swift
    - Creates FinishLaunch struct ro decode the finish launch response.

---
+++ Sources/Entities/FinishLaunch.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Isaac Tapia Contreras on 27/11/23.
+//
+
+import Foundation
+
+struct FinishLaunch: Decodable {
+    let id: String?
+    let link: String?
+    let number: Int?
+}
