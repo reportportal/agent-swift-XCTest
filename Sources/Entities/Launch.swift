7a8ccdbe53c56cc70337441dca93ec9e2d37a688
Isaac Tapia <itapia@esri.com>
2023-11-27 15:53:24 -0800

    Update Launch.swift
    -Updates Launch to current schema

--- Sources/Entities/Launch.swift
+++ Sources/Entities/Launch.swift
@@ -8,12 +8,50 @@
 
 import Foundation
 
-struct Launch: Decodable {
-  let id: String
-  let number: Int
-  let status: String?
+struct Launch: Decodable  {
+    let owner: String?
+    let share: Bool?
+    let id: Int
+    let uuid, name: String?
+    let number, startTime, endTime, lastModified: Int?
+    let status: String?
+    let statistics: Statistics?
+    let attributes: [Attributes?]?
+    let mode: String?
+    let analysing: [String]?
+    let approximateDuration: Float?
+    let hasRetries, rerun: Bool?
+    let metadata: Metadata?
+    let description: String?
+}
+
+struct Metadata: Decodable {
+    let rpClusterLastRun: String?
+}
+
+struct Statistics: Decodable {
+    let defects: [String: [String: Int32]]?
+    let executions: [String: Int32]?
 }
 
 struct LaunchListInfo: Decodable {
   let content: [Launch]
+  let page: Page?
+}
+
+struct Attributes: Decodable {
+    let key: String?
+    let value: String?
+}
+
+struct Page: Decodable {
+    let number: Int
+    let size: Int
+    let totalElements: Int
+    let totalPages: Int
+}
+
+struct LaunchID: Decodable {
+    let id: String
+    let number: Int
 }
