4785ecc575b80973aaf91066a637ad76b0f06251
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2024-04-30 13:16:55 +0300

    Update library metadata

--- Package.swift
+++ Package.swift
@@ -8,14 +8,14 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "ReportPortalAgent",
-            targets: ["ReportPortalAgent"]),
+            name: "ReportPortal",
+            targets: ["ReportPortal"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "ReportPortalAgent",
+            name: "ReportPortal",
             path: ".",
             sources: ["Sources"]),
     ]
