20a1916521bcb5c1f2714ea86fbbc672d6222faa
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2025-11-21 14:29:44 +0300

    Copyrights update

--- Package.swift
+++ Package.swift
@@ -1,3 +1,17 @@
+//  Copyright 2025 EPAM Systems
+//  
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//  
+//      https://www.apache.org/licenses/LICENSE-2.0
+//  
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
 // swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
@@ -7,10 +21,10 @@ let package = Package(
     name: "agent-swift-XCTest",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v15),
-        .macOS(.v12),
-        .tvOS(.v15),
-        .watchOS(.v8)
+        .iOS(.v18),
+        .macOS(.v14),
+        .tvOS(.v18),
+        .watchOS(.v11)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
